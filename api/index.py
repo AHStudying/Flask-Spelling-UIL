de4c0c470e85ecde5bfbab6b6ccd897184bf20d0
Calc-oholic <104602895+Calc-oholic@users.noreply.github.com>
2023-11-17 00:35:19 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -134,9 +134,21 @@ def pronounce_word():
 
     if current_word_idx < len(main_contest_words):
         word = main_contest_words[current_word_idx]
-        audio_data = generate_and_play_word(word)
-        return send_file(io.BytesIO(audio_data), mimetype='audio/mpeg', as_attachment=True, download_name=f'pronunciation_{word}.mp3')
+        audio_data, audio_filename = generate_and_play_word(word)
+
+        # Replace spaces with underscores in the filename
+        audio_filename = audio_filename.replace(" ", "_")
+
+        response_headers = {
+            'Content-Type': 'audio/mpeg',
+            'Content-Disposition': f'attachment; filename="{audio_filename}"',
+            'Content-Length': len(audio_data),
+        }
+
+        return send_file(io.BytesIO(audio_data), mimetype='audio/mpeg', as_attachment=True, download_name=audio_filename, response_headers=response_headers)
+
     else:
+        # Return an empty audio file if there are no more words
         return send_file(io.BytesIO(b""), mimetype='audio/mpeg', as_attachment=True, download_name='pronunciation_placeholder.mp3')
 
 if __name__ == "__main__":
